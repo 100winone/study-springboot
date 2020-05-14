4d95356cbaaa8a181e800841105e526f133347c2
100winone <53801551+100winone@users.noreply.github.com>
2020-05-14 19:34:48 +0900

    Day 4th - Adding index.mustache

---
+++ src/main/java/com/winone/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.winone.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/") // 머스테치 스타터가 컨트롤러에서 문자열 반환시 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
+    public String index() {
+        return "index"; // 여기서 index가 src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리..!!
+    }
+}
